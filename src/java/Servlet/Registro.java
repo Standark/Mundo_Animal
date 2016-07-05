e48793389640c5abcb603a307dae92f907c41043
MARIA DEL CARMEN <MARIA DEL CARMEN@GonzaloLF>
2016-07-05 11:25:11 +0200

    Arreglado cosas en el registro.

--- src/java/Servlet/Registro.java
+++ src/java/Servlet/Registro.java
@@ -5,7 +5,6 @@
  */
 package Servlet;
 
-
 import Modelo.Usuario;
 import ModeloDB.UsuarioDB;
 import ModeloDB.UsuarioDB;
@@ -31,7 +30,9 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "Registro", urlPatterns = {"/Registro"})
 
 public class Registro extends HttpServlet {
+
     private String url;
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -47,14 +48,11 @@ public class Registro extends HttpServlet {
         try {
             //Obtenemos la sesión actual
             HttpSession sesion = request.getSession();
-
             // En este caso de uso, el usuario NO tiene que haber iniciado sesion
             // por eso comprobamos que quien llegue a este servlet no este metido en sesion
-            if (sesion.getAttribute("nombre") == null) {
-
+            if (request.getParameter("nombre") != null) {
                 //Obtenemos el contenido de los cuadros de texto que nos llegan de la pagina jsp
                 // se utiliza request por que esta acción tiene el alcance de una única petición
-                int id =Integer.parseInt(request.getParameter("id"));
                 String nombre = request.getParameter("nombre");
                 String apellidos = request.getParameter("apellido");
                 String nick = request.getParameter("nick");
@@ -65,78 +63,68 @@ public class Registro extends HttpServlet {
                 String ciudad = request.getParameter("ciudad");
                 String provincia = request.getParameter("provincia");
                 int telefono = Integer.parseInt(request.getParameter("telefono"));
-                
-                
+
                 String dateInString = request.getParameter("nacimiento");
                 SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-                Date fechaNac=null;
-                
+                Date fechaNac = null;
 
-            try {
+                System.out.println("\nWoooooooooooooooooooooooooooo\n");
+                try {
+                    fechaNac = (Date) formatter.parse(dateInString);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
 
-		 fechaNac = (Date) formatter.parse(dateInString);
-		
-		
-
-	} catch (ParseException e) {
-		e.printStackTrace();
-	}
-               
                 //Password repetida no se recupera por que ya se ha comprobado con javascript en la pagina
                 //jsp que el usuario ha introducido la misma contraseña dos veces y no tiene sentido recuperarla 
                 //en este servlet
-
-
-              if(UsuarioDB.isUsuarioRegistrado(nick)){
+                if (UsuarioDB.isUsuarioRegistrado(nick)) {
                     request.setAttribute("textoError", "Ya hay un usuario registrado con ese nombre");
                     this.url = "/error.jsp";
                 }
-       
-                      
+
                 //Guardamos los datos en la BD, al insertar la función puede devolver dos valores, 0 si no ha
                 //conseguido insertarlo y 1 en caso contrario
-                Usuario nuevoUsuario = new Usuario( id,  nombre,  apellidos,  nick,  password,  direccion,  cp,  mail,  ciudad,  provincia,  telefono,  fechaNac);
-                
-               
+                //      Usuario nuevoUsuario = new Usuario(id, nombre, apellidos, nick, password, direccion, cp, mail, ciudad, provincia, telefono, fechaNac);
+                Usuario nuevoUsuario = new Usuario();
                 if (UsuarioDB.insertar(nuevoUsuario) == 0) {
                     request.setAttribute("textoError", "No se han guardado correctamente los datos");
                     this.url = "/error.jsp";
                 } else {
-            
-                         try (PrintWriter out = response.getWriter()) {
-                           /* TODO output your page here. You may use following sample code. */
-                           out.println("<!DOCTYPE html>");
-                           out.println("<html>");
-                           out.println("<head>");
-                           out.println("<title>Registro realizado</title>");            
-                           out.println("</head>");
-                           out.println("<body>");
-                           out.println("<h1>Te has registrado correctamente " + request.getContextPath() + "</h1>");
-                           out.println("</body>");
-                           out.println("</html>");
-                       }
-                  
+
+                    try (PrintWriter out = response.getWriter()) {
+                        /* TODO output your page here. You may use following sample code. */
+                        out.println("<!DOCTYPE html>");
+                        out.println("<html>");
+                        out.println("<head>");
+                        out.println("<title>Registro realizado</title>");
+                        out.println("</head>");
+                        out.println("<body>");
+                        out.println("<h1>Te has registrado correctamente " + request.getContextPath() + "</h1>");
+                        out.println("</body>");
+                        out.println("</html>");
+                    }
+
                 }
                 // Si el usuario que llega a este servlet esta registrado, no tiene sentido que lo vuelva a hacer, por eso
                 // redirigimos a su peril
             } else {
                 this.url = "/perfil.jsp";
             }
-            
+
             //En caso de algun error no previsto , redireccionamos a una pagina destinada a este fin
         } catch (Exception e) {
 
-            request.setAttribute("textoError", "Se ha producido un error, por favor vuelva a intentarlo");
+            request.setAttribute("textoError", "Inesperado, contacte con nosotros.");
             this.url = "/error.jsp";
-            
-        // Sea cual sea el resultado del procesamiento del servlet, se redirecciona a la página que se haya especificado
-        // en la varibale url
+
+            // Sea cual sea el resultado del procesamiento del servlet, se redirecciona a la página que se haya especificado
+            // en la varibale url
         } finally {
             RequestDispatcher respuesta = getServletContext().getRequestDispatcher(this.url);
             respuesta.forward(request, response);
         }
     }
-
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
